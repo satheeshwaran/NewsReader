c97a7d2bed3da78e10335434cc619c7f62c12aa3
satheeshwaran <satheesh4590@gmail.com>
2013-08-18 03:10:28 +0530

    Added Browse and Detail Screen files

---
+++ LNRDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  LNRDetailViewController.h
+//  LocalNewsReader
+//
+//  Created by Admin on 8/18/13.
+//  Copyright (c) 2013 Satheeshwaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LNRDetailViewController : UIViewController
+
+@end
---
+++ LocalNewsReader/Classes/ArticleBrowse/LNRBrowseViewController.h
@@ -0,0 +1,13 @@
+//
+//  LNRBrowseViewController.h
+//  LocalNewsReader
+//
+//  Created by Admin on 8/18/13.
+//  Copyright (c) 2013 Satheeshwaran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LNRBrowseViewController : UIViewController
+
+@end
